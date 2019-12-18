dbcc3951067b8985d8c6af71bad2320852200f3c
Phu Tran <camapcon87@gmail.com>
2019-12-18 14:23:02 +0700

    fix ios blank 12

--- www/js/app.js
+++ www/js/app.js
@@ -12,8 +12,6 @@ var theme = 'auto';
 if (document.location.search.indexOf('theme=') >= 0) {
   theme = document.location.search.split('theme=')[1].split('&')[0];
 }
-if(theme=='auto') theme = 'ios';
-//alert(theme);
 
 // Init App
 var app = new Framework7({
